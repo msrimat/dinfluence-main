63af056f74c9879dd8aa0217755a48eef6788ebf
Manu Srimat <msrimat@gmail.com>
2023-10-26 15:54:05 -0700

    changing experience mext day for now

--- components/sections/ExperiencesNextDay.tsx
+++ components/sections/ExperiencesNextDay.tsx
@@ -2,10 +2,11 @@ import ExperiencesNextDayCarousel from "../carousels/ExperiencesNextDayCarousel"
 import { getNextDay } from "../utils/FormatDate";
 import { client } from "../utils/Sanity";
 
+//&& date >= "${nextDay}"
 async function ExperiencesNextDay() {
   const nextDay = getNextDay();
   const data = await client.fetch(
-    `*[_type == 'experiences' && date >= "${nextDay}"] | order(_createdAt asc) {_id,title,rating,totalReviews,schedule,"imageUrl" : image.asset->url}`,
+    `*[_type == 'experiences' ] | order(_createdAt asc) {_id,title,rating,totalReviews,schedule,"imageUrl" : image.asset->url}`,
     {},
     { cache: "no-store" }
   );
